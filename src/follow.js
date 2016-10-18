ce665b22673691e29db761dedeb8af2b3de5b6e6
yanagiragi <qaz90473@gmail.com>
2016-10-18 15:57:39 +0800

    Update follow.js

--- src/follow.js
+++ src/follow.js
@@ -1,27 +1,37 @@
-const fs = require('graceful-fs')
 const querystring = require('querystring');
 const data = require('./data.js');
+const sanitize = require('sanitize-filename');
+const gracefulFs = require('graceful-fs')
+const fs = gracefulFs.gracefulify(require('fs'));
 
-const pix = data.getPixiv();
 const max = 9999;
-
+const pix = data.getPixiv();
 var storeindex = __dirname + "/Storage/getFollow";
 
 start();
 
 function start(){
+	console.log("Cool Start...");
 	process.chdir(storeindex);
 	pix.following().then(res => {
 		if(res.status == "success"){
-			for(var stat in res.response){
-				let id = res.response[stat].id;
-				let name = res.response[stat].name;
+			for(var stat = 0 ; stat < parseInt(res.count); stat++){
+				
+				var id = res.response[stat].id;
+				var name = sanitize(res.response[stat].name);
 				
 				if(!fs.existsSync(storeindex+"/"+name))
-					fs.mkdirSync(storeindex+"/"+name);
-				
-				preprocess("http://www.pixiv.net/member_illust.php?id="+id,storeindex+"/"+name+"/");
+					fs.mkdir(storeindex+"/"+name,function(err){
+						if(!err)
+							preprocess("http://www.pixiv.net/member_illust.php?id="+id,storeindex+"/"+name+"/");
+						else{
+							console.log(err);
+						}
+					});
+				else
+					preprocess("http://www.pixiv.net/member_illust.php?id="+id,storeindex+"/"+name+"/");
 			}
+			console.log("Warm Sleep, See U Tomorrow...");
 		}
 	});
 }
