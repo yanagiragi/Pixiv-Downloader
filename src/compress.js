e0e7b17b551948dd5a717f2aed7c2f22488e6ac7
yanagiragi <qaz90473@gmail.com>
2016-12-11 14:43:04 +0800

    Fixed Filename too long issue: use native tar instead

--- src/compress.js
+++ src/compress.js
@@ -8,13 +8,19 @@ compress();
 
 function compress(){
 	
-	var day = dateFormat(new Date(), "yyyymmdd");
+	//var day = dateFormat(new Date(), "yyyymmdd");
+	var day = (typeof process.argv[2] != "undefined")?process.argv[2]:dateFormat(new Date(), "yyyy-mm-dd");	// for date.pixiv.js
+	
 	var storeindex = __dirname + '/Storage/' + day + '/';
 	var tarindex = '../_Compress/' + day + '.tar';
 	
 	process.chdir(storeindex);
 
-	var dirDest = fs.createWriteStream(tarindex);
+	var proc = exec('tar -cf ' + tarindex + ' *');
+	proc.on('close', (code) => {
+		console.log('tar done with return code ' + code);
+	});
+	/*var dirDest = fs.createWriteStream(tarindex);
 
 	function onError(err) {
 		  console.error('An error occurred:', err)
@@ -33,5 +39,6 @@ function compress(){
 	    .on('error', onError)
         .pipe(packer)
         .pipe(dirDest);
+    */
 }
 
