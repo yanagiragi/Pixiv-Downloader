f4253439676556230c1d28aa6ff0184e8092f8ac
yanagiragi <qaz90473@gmail.com>
2017-02-15 21:24:44 +0800

    CleanUp

--- src/compress.js
+++ src/compress.js
@@ -1,14 +1,11 @@
 var fs = require('fs');
 var dateFormat = require('dateformat');
-var tar = require('tar');
-var fstream = require('fstream');
 const exec = require('child_process').exec;
 
 compress();
 
 function compress(){
 	
-	//var day = dateFormat(new Date(), "yyyymmdd");
 	var day = (typeof process.argv[2] != "undefined")?process.argv[2]:dateFormat(new Date(), "yyyy-mm-dd");	// for date.pixiv.js
 	
 	var storeindex = __dirname + '/Storage/' + day + '/';
@@ -20,25 +17,4 @@ function compress(){
 	proc.on('close', (code) => {
 		console.log('tar done with return code ' + code);
 	});
-	/*var dirDest = fs.createWriteStream(tarindex);
-
-	function onError(err) {
-		  console.error('An error occurred:', err)
-	}
-	
-	function onEnd() {
-		console.log('Packed!')
-	}
-	
-	var packer = tar.Pack({ noProprietary: true })
-		.on('error', onError)
-	    .on('end', onEnd);
-		
-	// This must be a "directory"
-	fstream.Reader({ path: storeindex, type: "Directory" })
-	    .on('error', onError)
-        .pipe(packer)
-        .pipe(dirDest);
-    */
 }
-
