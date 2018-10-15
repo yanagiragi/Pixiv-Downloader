d90c7512c48cb6b88b123c6fb726e9f0ba72ffc5
yanagiragi <qaz90473@gmail.com>
2018-10-15 15:05:44 +0800

    finish fetch user

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+	"env": {
+		"browser": true,
+		"commonjs": true,
+		"es6": true,
+		"node": true
+	},
+	"extends": "standard",
+	"plugins": [],
+	"rules": {
+		"indent": ["error", "tab"],
+		"no-tabs": 0,
+		"linebreak-style": ["error", "unix"],
+		"quotes": ["error", "single"],
+		"semi": ["error", "never"]
+	}
+};
