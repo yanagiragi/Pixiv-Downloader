f74a1b3f3c9c0219be25127191cd985c0c422a61
yanagiragi <qaz90473@gmail.com>
2018-10-17 09:11:26 +0800

    add template files

---
+++ src/data_template.js
@@ -0,0 +1,7 @@
+module.exports.getAccount = () => { return 'ACC' }
+
+module.exports.getPassword = () => { return 'PWD' }
+
+module.exports.getNodePath = () => { return '/home/XXX/.nvm/versions/node/v8.0.0/bin/node' }
+
+module.exports.getFilter = () => { return ['bl','腐向け'] }
