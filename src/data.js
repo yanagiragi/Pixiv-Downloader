b56476d517465a0392a48dbe1bcb0ba1c4e8efcd
yanagiragi <qaz90473@gmail.com>
2018-10-17 09:12:17 +0800

    cleanup

--- src/data.js
+++
@@ -1,7 +0,0 @@
-module.exports.getAccount = () => { return 'ACC' }
-
-module.exports.getPassword = () => { return 'PWD' }
-
-module.exports.getNodePath = () => { return '/home/XXX/.nvm/versions/node/v8.0.0/bin/node' }
-
-module.exports.getFilter = () => { return ['bl','腐向け'] }
