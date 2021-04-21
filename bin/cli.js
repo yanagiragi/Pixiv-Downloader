b5da63fc512d913294f03816c3a31a62153b0aa5
yanagiragi <qaz90473@gmail.com>
2021-04-21 22:23:15 +0800

    refactor: Add try catch to DealUserIllusts() and add comments

--- bin/cli.js
+++ bin/cli.js
@@ -7,11 +7,17 @@ const sanitize = require('sanitize-filename')
 
 const args = minimist(process.argv.slice(2))
 const sessionId = args.i
-const settingPath = args.s
-const cachePath = args.c
+const settingPath = args.s ?? 'setting.json'
+const cachePath = args.c ?? 'cache.json'
+const verbose = args.v === 'true'
 
 const StoragePath = path.join(__dirname, 'Storage')
 
+/**
+ * Create dir if not exists
+ * @param {string}} - folder path
+ * @returns {Void}
+ */
 function EnsureDirExist(folder)
 {
 	if (!fs.existsSync(folder)) {
@@ -20,25 +26,39 @@ function EnsureDirExist(folder)
 }
 
 /**
- * Return Promise when pixiv image download completes
- * @return {string} output - saved path of the image
- * @param {string} url 
- * @param {string} filename 
+ * Wrapper to fetch pixiv apis
+ * @param {string} url - url of the pixiv api
+ * @param {object} [option] - additional options
+ * @returns {promise}
  */
- async function GetPixivImage (url, storePath, filename) {
+async function FetchFromPixiv(url, option = {}) 
+{
+	return fetch(url, Object.assign(option, {
+		'headers' : {
+			'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0',
+			'Accept': 'application/json' ,
+			'Referer': 'https://www.pixiv.net/',
+			'Pragma': 'no-cache',
+			'Cookie': `PHPSESSID=${sessionId}`
+		}
+	}))
+}
+
+/**
+ * Return Promise when pixiv image download completes
+ * @param {string} url - url of the image to get
+ * @param {string} storePath - folder path to be store the image
+ * @param {string} filename - filename of the image to be stored
+ * @returns {boolean} Is success or not. if skipped return true.
+ */
+async function GetPixivImage (url, storePath, filename) {
 	try {
 		EnsureDirExist(storePath)
 		const savePath = path.join(storePath, filename)
 		if (fs.existsSync(savePath)) {
 			return true
 		}
-		const response = await fetch(url, { 
-			encoding : 'binary',
-			headers: { 
-				 'Referer': 'http://www.pixiv.net/'
-			}, 
-			timeout: 1000 * 100 
-		})
+		const response = await fetch(url, { encoding : 'binary', timeout: 1000 * 100 })
 		if (!response.ok) return false
 		const body = await response.buffer()
 		fs.writeFileSync(savePath, body, 'binary')
@@ -50,19 +70,11 @@ function EnsureDirExist(folder)
 	}	
 }
 
-async function FetchFromPixiv(url) 
-{
-	return fetch(url, {
-		'headers' : {
-			'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0',
-			'Accept': 'application/json' ,
-			'Referer': 'https://www.pixiv.net/',
-			'Pragma': 'no-cache',
-			'Cookie': `PHPSESSID=${sessionId}`
-		}
-	})
-}
-
+/**
+ * Return fetch results of the illustId
+ * @param {string} illustId - id of the illust
+ * @returns {[string, [string, string]]} [ title of the image, [ prefix of the url of the image, postfix of the url of the image ] ]
+ */
 async function GetImageUrlAndTitle(illustId) 
 {
 	try {
@@ -70,57 +82,76 @@ async function GetImageUrlAndTitle(illustId)
 		const data = await resp.text()
 		const raw = data.match(/id="meta-preload-data" content='(.*)'>/)
 		const json = JSON.parse(raw[1])
-		const src = json?.illust[illustId]?.urls?.original
-		const title = json?.illust[illustId]?.title
-		const filename = src.match(/\d+_p/)[0]
+		const src = json?.illust?.[illustId]?.urls?.original
+		const title = json?.illust?.[illustId]?.title
+		const filename = src.match(/\d+_[p|ugoira]/)[0]
 		const prefix = src.substring(0, src.indexOf(filename) + filename.length)
 		const postfix = src.substring(src.indexOf(filename) + filename.length + 1, src.length)
 		return [sanitize(title), [prefix, postfix]]
 	} catch (err) {
 		console.log(err)
-		return [null, null];
+		return [null, null]
 	}
 }
 
+/**
+ * Downloads all image of a user
+ * @param {Object} setting - data to be download
+ * @param {Object} setting.id - id of the user, e.g. 2168501
+ * @param {Object} setting.name - name of the user, e.g. のみや
+ * @param {[Object]} caches - cache of the saved image to accerlate the download progress
+ * @returns {[Object]} the updated cache
+ */
 async function DealUserIllusts(setting, caches) {
 	const cache = [...caches]
 	const id = setting.id
 	const name = setting.name
-
-	const saveFolderPath = path.join(StoragePath, name)
-	EnsureDirExist(StoragePath)
-	EnsureDirExist(saveFolderPath)
 	
-	const resp = await FetchFromPixiv(`https://www.pixiv.net/ajax/user/${id}/profile/all?lang=ja`)
-	const data = await resp.json()
-	const illustIds = Object.entries(data?.body?.illusts).map(el => el[0])
-
-	for(let i = 0; i < illustIds.length; ++i) {
-		const illustId = illustIds[i]
-		if (cache.includes(illustId)) {
-			continue
-		}
-
-		const [title, [prefix, postfix]] = await GetImageUrlAndTitle(illustId)
-		if (title == null) {
-			console.log(`Error when fetching ${illustId}. Skipped`)
-			continue
-		}
-
-		console.log(`Downloading ${i+1}/${illustIds.length}: ${illustId}`)
+	try {
+		const saveFolderPath = path.join(StoragePath, `${id}-${sanitize(name)}`)
+		EnsureDirExist(StoragePath)
+		EnsureDirExist(saveFolderPath)
 		
-		let count = 0, canDonwload = true;
-		do {
-			canDonwload = await GetPixivImage(`${prefix}${count}${postfix}`, path.join(saveFolderPath, title), `${illustId} - ${count}${postfix}`)
-			count += 1
-		} while (canDonwload)
+		const resp = await FetchFromPixiv(`https://www.pixiv.net/ajax/user/${id}/profile/all?lang=ja`)
+		const data = await resp.json()
+		const illustIds = Object.entries(data?.body?.illusts).map(el => el[0])
 
-		cache.push(illustId)
+		for(let i = 0; i < illustIds.length; ++i) {
+			const illustId = illustIds[i]
+			if (cache.includes(illustId)) {
+				if (verbose)
+					console.log(`Skip ${illustId}`)
+				continue
+			}
+
+			const [title, [prefix, postfix]] = await GetImageUrlAndTitle(illustId)
+			if (title == null) {
+				console.log(`Error when fetching ${illustId}. Skipped`)
+				continue
+			}
+
+			console.log(`Downloading ${i+1}/${illustIds.length}: ${illustId}`)
+			
+			let count = 0, canDonwload = true
+			do {
+				canDonwload = await GetPixivImage(`${prefix}${count}${postfix}`, path.join(saveFolderPath, `${illustId}-${title}`), `${illustId}-${count}${postfix}`)
+				count += 1
+			} while (canDonwload)
+
+			cache.push(illustId)
+		}
+	} catch (err) {
+		console.log(err)
 	}
 
 	return cache
 }
 
+/**
+ * Entrance of the program
+ * @param {Void}
+ * @returns {Void}
+ */
 async function Run() {	
 	try {
 		const settings = JSON.parse(fs.readFileSync(settingPath))
