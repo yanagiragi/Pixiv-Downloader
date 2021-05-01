baec6ee9ab42ab15c9538d02eab41c40ef1446b1
yanagiragi <qaz90473@gmail.com>
2021-05-01 23:16:39 +0800

    fix: update docker_run.sh

--- bin/docker_run.sh
+++ bin/docker_run.sh
@@ -11,11 +11,10 @@ if [ -z ${SESSION+x} ]; then
 else 
 	# use docker in rootless mode
 	cd ~/Pixiv-Downloader/bin && \
-	~/bin/docker run -itd \
+	~/bin/docker run -it --rm \
 		-v $(pwd)/Storage:$REPO_ROOT/bin/Storage \
 		-v $(pwd)/data:$REPO_ROOT/bin/data \
 		$DOCKER_IMAGE_TAG \
 		bash -c "cd $REPO_ROOT/bin && /usr/local/bin/node cli.js -s  $SETTINGJSON -c $CACHEJSON -i $SESSION"
-		#bash
 fi
 
